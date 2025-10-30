78a2dcbb3adf29c69f5a46c71efeed18b05ca784
BenicioLadines <benicioladines814@gmail.com>
2025-10-29 21:33:58 -0400

    MINIGAME DONE LETS GOOOO

--- Assets/Beniciooo/Heartbeat/HeartbeatManager.cs
+++ Assets/Beniciooo/Heartbeat/HeartbeatManager.cs
@@ -7,61 +7,83 @@ namespace Benicio
     {
         public float heartDeplete;
         public float heartAdd;
-        float heartMeter = 0;
+        float heartMeter;
         public float winValue;
         public Image debugMeter;
         public Image debugWinVal;
         bool finished = false;
         public GameObject heart;
-        public float finishTimer;
+        public SpriteRenderer heartSprite;
+        Animator animator;
+        public float popValue;
         public override void OnEndMicroGame()
         {
+
         }
 
         public override void OnStartMicroGame()
         {
-
+            animator.Play("intro");
         }
 
         private void Start()
         {
+            heartMeter = winValue;
             debugWinVal.fillAmount = winValue;
+            animator = GetComponent<Animator>();
         }
 
         // Update is called once per frame
-        void Update()
+        new void Update()
         {
             base.Update();
-            
-            debugMeter.fillAmount = heartMeter;
-            if (finished) return;
-            finishTimer += Time.deltaTime;
-            if(finishTimer > 9f && !finished)
+            if (IsGameActive())
             {
-                finished = true;
-                if(heartMeter > winValue)
+                heart.transform.localScale = Vector3.one * (1 + heartMeter);
+                animator.SetFloat("shakeVal", heartMeter);
+                debugMeter.fillAmount = heartMeter;
+                if (finished) return;
+                if (GetCurrentTime() < GetMaxTime() * 0.1f && !finished)
                 {
-                    win = true;
-                    debugMeter.color = Color.blue;
+                    finished = true;
+                    animator.Play("outro");
+                    if (heartMeter > winValue)
+                    {
+                        animator.Play("livingLoop");
+                        win = true;
+                        debugMeter.color = Color.blue;
+                    }
+
+                }
+
+                if (heartMeter > winValue)
+                {
+                    heartSprite.color = Color.Lerp(Color.white, Color.red, heartMeter - winValue);
+                }
+                else
+                {
+                    heartSprite.color = Color.white;
+                }
+
+                if (heartMeter > 0)
+                {
+                    heartMeter -= heartDeplete * Time.deltaTime;
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    heartMeter += heartAdd;
+                    animator.Play("heartbeatAnim", 0, 0);
+                }
+
+                if (heartMeter > popValue)
+                {
+                    win = false;
+                    finished = true;
+                    animator.Play("heartPop");
+                    debugMeter.color = Color.red;
                 }
             }
-
-            if(heartMeter > 0)
-            {
-                heartMeter -= heartDeplete * Time.deltaTime;
-            }
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                heartMeter += heartAdd;
-            }
-
-            if( heartMeter > 1)
-            {
-                win = false;
-                finished = true;
-
-                debugMeter.color = Color.red;
-            }
+           
         }
     }
 }
